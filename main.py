e2affbf2793609ec172b45dd3b9c0f2f795f9d3c
Makar Dudnik <makardudnik3@gmail.com>
2023-12-09 00:34:48 +0700

    GUI

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import (QApplication,
+                            QMainWindow,
+                            QPushButton,
+                            QLabel, 
+                            QLineEdit, 
+                            QVBoxLayout, 
+                            QWidget)
+
+
+import sys
+
 def is_cbs(seq):
     stack = []
     for i in seq:
@@ -26,4 +38,50 @@ def need_to_move(seq):
     b = stack.count(')')
     return max(a,b)
 
-print(need_to_move("(()))("))
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setStyleSheet("background-color: gray;")
+        self.setWindowTitle("CBSCheck v1 (GUI EDITION GOTY 20002)")
+
+        self.setFixedSize(QSize(800, 640))
+
+        self.helpText = QLabel()
+        self.helpText.setText("To use the program:"
+            "\nInput the brace sequence in the field below and press enter or the check button.")
+        self.helpText.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+
+        self.label = QLabel()
+
+        self.button = QPushButton()
+        self.button.setText("check")
+
+        self.input = QLineEdit()
+        self.input.textChanged
+
+        layout = QVBoxLayout()
+
+        layout.addWidget(self.helpText,0,Qt.AlignmentFlag.AlignHCenter)
+        layout.addWidget(self.input,0,Qt.AlignmentFlag.AlignTop)
+        layout.addWidget(self.label,1,Qt.AlignmentFlag.AlignHCenter|Qt.AlignmentFlag.AlignTop)
+        layout.addWidget(self.button,1, Qt.AlignmentFlag.AlignBottom)
+
+        self.button.clicked.connect(self.get_result)
+
+        self.input.returnPressed.connect(self.get_result)
+        
+        container = QWidget()
+        container.setLayout(layout)
+
+        self.setCentralWidget(container)
+
+    def get_result(self):
+        self.label.setText(str(is_cbs(self.input.text())))
+
+program = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+program.exec()
